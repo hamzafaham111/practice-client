c51e42a6278c10680941f491e3f269f454c75b99
MacBook Pro <macbookpro@10.10.12.25>
2024-11-08 02:51:34 +0500

    changed backend url

--- src/apoloClient.js
+++ src/apoloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 // Set up Apollo Client
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql', // URL of your GraphQL endpoint
+  uri: 'http://54.153.204.110/graphql', // URL of your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
